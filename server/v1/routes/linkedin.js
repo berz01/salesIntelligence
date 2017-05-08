679c4ba8efaff71605685f81034bbe3f42a0532f
John Sagan <jsagan423@gmail.com>
2017-05-08 15:58:03 -0400

    got successful response from linkedIn; calling it a day

--- server/v1/routes/linkedin.js
+++ server/v1/routes/linkedin.js
@@ -2,6 +2,7 @@ var express = require('express');
 var http = require('http');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var request = require('request');
 var api = express.Router();
 
 api.use(bodyParser.json()); // support json encoded bodies
@@ -10,9 +11,15 @@ api.use(bodyParser.urlencoded({
 }));
 
 api.get("/maincall",  function(req, res){
-  console.log("hit linkedin");
-  var data = {"stuff":"to look at"};
-  res.send(data);
+
+
+  request({
+    url: "https://api.linkedin.com/v1/people/~?oauth2_access_token=AQWIUJK4biSi45Ds6O5IG7ykcwIpIO3CYR6FqHGk5rWfSVlZv3Eo-WU9K_Aq0R3jLnUGlx6uALd_etUem2xaKyfJJ51vG7FArSMijzmRcdMEdXiEC8wdOy6piulNx4HNQqsfErxMIgluEhgzxpPYPrQjOj6kBP3s6ydmVEaBX4WuxYlAGM4&format=json",
+    method: "GET",
+    json: true
+  }, function(error, response, body) {
+    res.send(response.body);
+  });
 });
 
 module.exports = api;
