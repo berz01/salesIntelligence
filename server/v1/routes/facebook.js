01ed143e5c89ec56058732c543bcf6a4e0264cb5
Barrett Davis <barrettdavis01@gmail.com>
2017-05-09 11:59:26 -0400

    - barrett commit

--- server/v1/routes/facebook.js
+++ server/v1/routes/facebook.js
@@ -2,6 +2,7 @@ var express = require('express');
 var http = require('http');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var request = require('request');
 var api = express.Router();
 
 api.use(bodyParser.json()); // support json encoded bodies
@@ -9,12 +10,42 @@ api.use(bodyParser.urlencoded({
   extended: true
 }));
 
-api.get("/maincall", function(req, res) {
-  console.log("hit facebook");
-  var data = {
-    "stuff": "to look at"
-  };
-  res.send(data);
+var accessToken = "EAASKn0f7tI0BALpi0kVEVQHGkQfiZBraZAHMscEqyYO9f7PbAqFAy3or535vYUqbdSlJfkrI0a8tC8ARROQU0bI86z3ZBaIsc1uymZC2Tq8JH4pJrZAfuTGew0Bu1GQSBLUBRKPF5tFFfb7J8ZAt45xNz7TwA2dzaOdGedO3oiuUR0ZBwmRafYonatdroTNiIMZD"
+
+api.get("/profile", function(req, res) {
+
+      request({
+          url: "https://graph.facebook.com/me?" + "access_token=" + accessToken,
+          method: "GET",
+          json: true
+
+
+
+      }, function(error, response, body) {
+          console.log(body);
+          console.log("name" + body.name)
+          res.send(body);
+
+      });
+        console.log("https://graph.facebook.com/me?" + accessToken);
 });
+
+// api.get('/profile', function(req, res) {
+//
+//     request({
+//         url: "https://graph.facebook.com/me",
+//         method: "GET",
+//         json: true,
+      //     headers: {
+      //   'Accept': 'application/json',
+      //   'Accept-Charset': 'utf-8',
+      //   'User-Agent': 'my-reddit-client'
+      // }
+//
+//     }, function(error, response, body) {
+//         console.log(response.body);
+//     });
+//
+// });
 
 module.exports = api;
