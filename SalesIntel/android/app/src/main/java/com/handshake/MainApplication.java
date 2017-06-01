0172c9ed4734dafd5da0fc80cba1666927f0de71
ereio <Tte11@my.fsu.edu>
2017-06-01 16:38:21 -0400

    stuff

--- SalesIntel/android/app/src/main/java/com/handshake/MainApplication.java
+++ SalesIntel/android/app/src/main/java/com/handshake/MainApplication.java
@@ -37,6 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FBSDKPackage(),
             new FBSDKPackage(mCallbackManager),
             new ReactMaterialKitPackage(),
             new VectorIconsPackage()
